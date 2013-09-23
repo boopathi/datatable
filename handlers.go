dda76046d1477b79732a723b222a61cce028fbdd
Boopathi Rajaa <me@boopathi.in>
2013-09-23 22:33:43 +0530

    Adding templates and concurrency to parse the data fetched from DB

--- handlers.go
+++ handlers.go
@@ -2,8 +2,11 @@ package main
 
 import (
   "fmt"
+  "bytes"
+  "html/template"
   "net/http"
   "github.com/gorilla/mux"
+  "labix.org/v2/mgo/bson"
   "strings"
 )
 
@@ -19,7 +22,89 @@ func ListViews(w http.ResponseWriter, r *http.Request) {
   w.Write(page)
 }
 
+func DT_GetHeaders(class string, h chan<- []string, e chan<- error) {
+  ha, err := GetTableDesc(class)
+  if err != nil {
+    e<- err
+    return
+  }
+  Headers := strings.Split(ha.Cols, ",")
+  h<- Headers
+}
+
+func ParseQuark(q Quark, b chan<- [][]string) {
+  var Body [][]string
+  lines := strings.Split(q.Value, "\n")
+  for _, line := range lines {
+    if line == "" { continue }
+    Body = append(Body, strings.Split(line,","))
+  }
+  b<- Body
+}
+
 func ViewHandler(w http.ResponseWriter, r *http.Request) {
+  var err error
+  vars := mux.Vars(r)
+  class := vars["view"]
+  if class == "" {
+    w.Write([]byte("Invalid Class"))
+    return
+  }
+
+  //Throw Page initial content first
+  page,err := getPage("header", nil)
+  if err != nil { fmt.Println(err); return }
+  w.Write(page)
+
+  head := make(chan []string)
+  ehead := make(chan error)
+  go DT_GetHeaders(class,head,ehead)
+
+  c := DB.Db.C(class)
+  var q Quark
+  body := make(chan [][]string)
+  iter := c.Find(bson.M{}).Iter()
+  count := 0
+  for iter.Next(&q) {
+    count = count + 1
+    go ParseQuark(q, body)
+  }
+
+  var buf bytes.Buffer
+  //wait to throw head
+  select {
+  case Headers := <-head :
+    t, err := template.ParseFiles(Config.TmplDir + "/dt_header.html")
+    if err != nil { fmt.Println(err); return}
+    t.Execute(&buf, Headers)
+    w.Write(buf.Bytes())
+
+  case err = <-ehead:
+    fmt.Println(err)
+    return
+  }
+
+  //Now the middle portion
+  page, err = getPage("middle", nil)
+  if err != nil { fmt.Println(err); return }
+  w.Write(page)
+
+  //Now Throw Body
+  for i:=0;i<count;i++ {
+    Body := <-body
+    t, err := template.ParseFiles(Config.TmplDir + "/dt_body.html")
+    if err != nil { fmt.Println(err); return }
+    t.Execute(&buf, Body)
+    w.Write(buf.Bytes())
+  }
+
+  page,err = getPage("footer", nil)
+  if err != nil { fmt.Println(err); return }
+  w.Write(page)
+
+}
+
+func ViewHandler_Old(w http.ResponseWriter, r *http.Request) {
   vars := mux.Vars(r)
   class := vars["view"]
   if class == "" {
