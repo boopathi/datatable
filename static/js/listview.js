df468c707a3e64a1149be1447da1b36a7cfb4739
Boopathi Rajaa <me@boopathi.in>
2013-09-25 00:36:31 +0530

    Improved performance of datatable.js :) ... Really Really Significant

--- static/js/listview.js
+++ static/js/listview.js
@@ -1,6 +1,10 @@
 $(function() {
   'use strict';
   $("#datatable").dataTable({
+    "bProcessing": true,
+    "bDeferRender": true,
+    "aaData": aaData,
+    "aoColumns": aoColumns,
     "sDom": "<'row'<'span6'l><'span6'f>r>t<'row'<'span6'i><'span6'p>>",                                                                                                   
     "sPaginationType": "bootstrap",                                                                                                                                       
     "oLanguage": {                                                                                                                                                        
