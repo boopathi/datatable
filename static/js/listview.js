78139bd05afc5d7bd4f486dbb4bcc2396c4261ee
Boopathi Rajaa <me@boopathi.in>
2013-09-26 19:37:16 +0530

    Changing some views

--- static/js/listview.js
+++ static/js/listview.js
@@ -5,7 +5,7 @@ $(function() {
     "bDeferRender": true,
     "aaData": aaData,
     "aoColumns": aoColumns,
-    "sDom": "<'row'<'span6'l><'span6'f>r>t<'row'<'span6'i><'span6'p>>",                                                                                                   
+    "sDom": "<'row'<'span6'l><pf>r>t<'row'<'span6'i>>",                                                                                                   
     "sPaginationType": "bootstrap",                                                                                                                                       
     "oLanguage": {                                                                                                                                                        
       "sLengthMenu": "_MENU_ records per page"                                                                                                                            
